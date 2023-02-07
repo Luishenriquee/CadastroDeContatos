9d98e31f276c2f64031ecb792c59fc1b412ecfff
Luishenriquee <iluishenriquebr@gmail.com>
2023-02-06 21:08:57 -0300

    Adição do DataAnnotations para validação

--- CadastroDeContatos/Models/ContatoModel.cs
+++ CadastroDeContatos/Models/ContatoModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,10 +10,15 @@ namespace CadastroDeContatos.Models
     {
         public int Id { get; set; }
 
+        [Required(ErrorMessage ="Digite o nome do contato")]
         public string Nome { get; set; }
 
+        [Required(ErrorMessage ="Digite o e-mail do contato")]
+        [EmailAddress(ErrorMessage ="E-mail inválido!")]
         public string Email { get; set; }
 
+        [Required(ErrorMessage ="Digite o celular do contato")]
+        [Phone(ErrorMessage ="Celular inválido")]
         public string Celular { get; set; }
     }
 }
