31951bdd36a0b75a1c13f68a16b66c5fa9f4369b
Luishenriquee <iluishenriquebr@gmail.com>
2023-02-13 20:41:49 -0300

    Tradução para o Português

--- CadastroDeContatos/wwwroot/js/site.js
+++ CadastroDeContatos/wwwroot/js/site.js
@@ -3,8 +3,33 @@
 
 // Write your JavaScript code.
 
-$(document).ready(function () {
-    $('#table-contato').DataTable();
+$('#table-contato').DataTable({
+    "ordering": true,
+    "paging": true,
+    "searching": true,
+    "oLanguage": {
+        "sEmptyTable": "Nenhum registro encontrado!",
+        "sInfo": "Mostrar _START_ até _END_ de _TOTAL_ registros",
+        "sInfoEmpty": "Mostrar 0 até 0 de 0 Registros",
+        "sInfoFiltered": "(Filtrar de _MAX_ total registros)",
+        "sInfoPostFix": "",
+        "sInfoThousands": ".",
+        "sLengthMenu": "Mostrar _MENU_ registros por página",
+        "sLoadingRecords": "Carregando...",
+        "sProcessing": "Processando...",
+        "sZeroRecords": "Nenhum registro encontrado!",
+        "sSearch": "Pesquisar",
+        "oPaginate": {
+            "sNext": "Próximo",
+            "sPrevious": "Anterior",
+            "sFirst": "Primeiro",
+            "sLast": "Último"
+        },
+        "oAria": {
+            "sSortAscending": ": Ordenar colunas de forma ascendente",
+            "sSortDescending": ": Ordenar colunas de forma descendente"
+        }
+    }
 });
 
 $('.Close-alert').click(function () {
