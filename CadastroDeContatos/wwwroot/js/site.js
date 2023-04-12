737246b3adf089001e3f7be2e58da55cf4874220
Luishenriquee <iluishenriquebr@gmail.com>
2023-04-12 19:43:13 -0300

    Adição da controller Usuário e da tela Usuário

--- CadastroDeContatos/wwwroot/js/site.js
+++ CadastroDeContatos/wwwroot/js/site.js
@@ -1,36 +1,38 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿$(document).ready(function () {
+    getDatatable('#table-contato');
+    getDatatable('#table-usuario');
+})
 
-// Write your JavaScript code.
-
-$('#table-contato').DataTable({
-    "ordering": true,
-    "paging": true,
-    "searching": true,
-    "oLanguage": {
-        "sEmptyTable": "Nenhum registro encontrado!",
-        "sInfo": "Mostrar _START_ até _END_ de _TOTAL_ registros",
-        "sInfoEmpty": "Mostrar 0 até 0 de 0 Registros",
-        "sInfoFiltered": "(Filtrar de _MAX_ total registros)",
-        "sInfoPostFix": "",
-        "sInfoThousands": ".",
-        "sLengthMenu": "Mostrar _MENU_ registros por página",
-        "sLoadingRecords": "Carregando...",
-        "sProcessing": "Processando...",
-        "sZeroRecords": "Nenhum registro encontrado!",
-        "sSearch": "Pesquisar",
-        "oPaginate": {
-            "sNext": "Próximo",
-            "sPrevious": "Anterior",
-            "sFirst": "Primeiro",
-            "sLast": "Último"
-        },
-        "oAria": {
-            "sSortAscending": ": Ordenar colunas de forma ascendente",
-            "sSortDescending": ": Ordenar colunas de forma descendente"
+function getDatatable(id) {
+    $(id).DataTable({
+        "ordering": true,
+        "paging": true,
+        "searching": true,
+        "oLanguage": {
+            "sEmptyTable": "Nenhum registro encontrado!",
+            "sInfo": "Mostrar _START_ até _END_ de _TOTAL_ registros",
+            "sInfoEmpty": "Mostrar 0 até 0 de 0 Registros",
+            "sInfoFiltered": "(Filtrar de _MAX_ total registros)",
+            "sInfoPostFix": "",
+            "sInfoThousands": ".",
+            "sLengthMenu": "Mostrar _MENU_ registros por página",
+            "sLoadingRecords": "Carregando...",
+            "sProcessing": "Processando...",
+            "sZeroRecords": "Nenhum registro encontrado!",
+            "sSearch": "Pesquisar",
+            "oPaginate": {
+                "sNext": "Próximo",
+                "sPrevious": "Anterior",
+                "sFirst": "Primeiro",
+                "sLast": "Último"
+            },
+            "oAria": {
+                "sSortAscending": ": Ordenar colunas de forma ascendente",
+                "sSortDescending": ": Ordenar colunas de forma descendente"
+            }
         }
-    }
-});
+    });
+}
 
 $('.Close-alert').click(function () {
     $('.alert').hide('hide');
