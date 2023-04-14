170c4175591c35b2499c1bfb12a8b2467752029e
Luishenriquee <iluishenriquebr@gmail.com>
2023-04-14 13:08:31 -0300

    Adição do método para adicionar e listar os usuário e adição da página de usuários.

--- CadastroDeContatos/Controllers/UsuarioController.cs
+++ CadastroDeContatos/Controllers/UsuarioController.cs
@@ -1,6 +1,7 @@
 ﻿using CadastroDeContatos.Models;
 using CadastroDeContatos.Repositorio;
 using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 
 namespace CadastroDeContatos.Controllers
@@ -19,6 +20,30 @@ namespace CadastroDeContatos.Controllers
             List<UsuarioModel> usuarios = _usuarioRepositorio.ListarUsuario();
 
             return View(usuarios);
+        }
+
+        public IActionResult Criar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Criar(UsuarioModel usuario)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                    return View(usuario);
+
+                _usuarioRepositorio.Adicionar(usuario);
+                TempData["MensagemSucesso"] = "Usuário cadastrado com sucesso!";
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+                TempData["MensagemErro"] = $"Erro ao cadastrar o usuário, {ex.Message}";
+                return RedirectToAction("Index");
+            }
         }
     }
 }
--- CadastroDeContatos/Models/UsuarioModel.cs
+++ CadastroDeContatos/Models/UsuarioModel.cs
@@ -1,15 +1,26 @@
 ﻿using CadastroDeContatos.Enums;
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace CadastroDeContatos.Models
 {
     public class UsuarioModel
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Digite o nome do usuário")]
         public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Digite o login do usuário")]
         public string Login { get; set; }
-        public string Senha { get; set; }
+
+        [Required(ErrorMessage = "Digite o e-mail do contato")]
+        [EmailAddress(ErrorMessage = "E-mail inválido!")]
         public string Email { get; set; }
+
+        [Required(ErrorMessage = "Digite a senha do usuário")]
+        public string Senha { get; set; }
+
         public PerfilAcesso PerfilAcesso { get; set; }
         public DateTime DataCadastro { get; set; }
         public DateTime? DataAtualizacao { get; set; }
